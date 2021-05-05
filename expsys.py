e9197ec2d842d1445236722706cf3127ffe4d409
yohan thomas <yohan.thomas27@hotmail.com>
2021-05-05 13:02:20 +0200

    main adapted for visual

--- expsys.py
+++ expsys.py
@@ -10,15 +10,13 @@ import pandas as pd
 def main():
     args = parse_args()
 
-    params = Setup(args.modify, args.visual, args.input_file)
+    params = Setup(args.modify, args.visual, args.display_trees, args.input_file)
     adj_matrix = pd.DataFrame(index=ALL_FACTS) # matrix indexes pre-set so we juste have to add columns by columns (rules)
     facts = Facts()
     rules = Rules()
     env = Env(facts, rules, adj_matrix)
     
     parse_input(params, env)
-
-
 
 if __name__ == "__main__":
     main()
